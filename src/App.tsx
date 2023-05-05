70c1058280582b2be3456991a1db20e01e9c7ed4
Ben Muschol <benjamin.muschol@airtable.com>
2023-05-05 16:59:33 -0700

    Initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        initialViewState={{
+          longitude: -73.935242,
+          latitude: 40.73061,
+          zoom: 9,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
     </div>
   );
 }
