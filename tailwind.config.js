2c79fa3747b6b704fa0325f5ff0638f070dfef93
Ben Muschol <benjamin.muschol@airtable.com>
2023-05-14 19:28:03 -0400

    Remove some stuff

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    container: { center: true },
-    extend: {},
-  },
-  plugins: [],
-};
