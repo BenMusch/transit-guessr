1554d2a838851d5b6a6f2f636271af15566ab797
Ben Muschol <benjamin.muschol@airtable.com>
2023-05-20 18:41:26 -0400

    some progress

--- src/LineBadge.tsx
+++ src/LineBadge.tsx
@@ -2,16 +2,36 @@ import type { Line } from "./data/lines";
 import { BACKGROUND_COLOR_BY_TRUNK_LINE } from "./data/lines";
 import "./LineBadge.css";
 
-export function LineBadge(props: { line: Line; small?: boolean }) {
-  const { line, small } = props;
+export function LineBadge(props: {
+  line: Line;
+  small?: boolean;
+  medium?: boolean;
+  greyscale?: boolean;
+}) {
+  const { line, small, medium, greyscale } = props;
+  const classes = ["station-line"];
+  if (small) {
+    classes.push("station-line-small");
+  }
+  if (medium) {
+    classes.push("station-line-medium");
+  }
+  if (greyscale) {
+    classes.push("station-line-greyscale");
+  }
+  if (line.express) {
+    classes.push("express");
+  } else {
+    classes.push("local");
+  }
   return (
     <div
       style={{
-        backgroundColor: BACKGROUND_COLOR_BY_TRUNK_LINE[line.trunkLine],
+        backgroundColor: greyscale
+          ? "#64748b"
+          : BACKGROUND_COLOR_BY_TRUNK_LINE[line.trunkLine],
       }}
-      className={`station-line${small ? " station-line-small" : ""}${
-        line.express ? " express" : " local"
-      }`}
+      className={classes.join(" ")}
     >
       <span className="station-line-name">{line.name}</span>
     </div>
