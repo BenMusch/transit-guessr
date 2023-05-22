13ebaf7d5cfe10d45cc1bec472d33bc5f8ccaa14
Ben Muschol <benjamin.muschol@airtable.com>
2023-05-22 14:17:39 -0400

    WIP

---
+++ src/operators/mbta/types.tsx
@@ -0,0 +1,17 @@
+import { IStation, ILine } from "../types";
+
+export enum MbtaTrunkLine {
+  RED = "red",
+  BLUE = "blue",
+  GREEN = "green",
+  ORANGE = "orange",
+  SILVER = "silver",
+}
+
+export enum MbtaLineName {
+  RED = "red",
+  BLUE = "blue",
+  GREEN_B = "greenB",
+  ORANGE = "orange",
+  SILVER = "silver",
+}
